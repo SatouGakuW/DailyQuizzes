321f3c5e2da618fd0d943f14f06110d2788f9728
yangshihao-arron <1530910726@qq.com>
2023-05-10 16:53:50 +0800

    daily pratice 2023/5/10

---
+++ yshCode/src/daimasuixianglu/d2/Q209.java
@@ -0,0 +1,17 @@
+package daimasuixianglu.d2;
+
+public class Q209 {
+    public int minSubArrayLen(int target, int[] nums) {
+        int res = Integer.MAX_VALUE, left = 0, sum = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while (sum >= target) {
+                res = Math.min(res, right - left + 1);
+                sum -= nums[left];
+                left++;
+            }
+
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+}
---
+++ yshCode/src/daimasuixianglu/d2/Q977.java
@@ -0,0 +1,22 @@
+package daimasuixianglu.d2;
+
+public class Q977 {
+    public int[] sortedSquares(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return nums;
+        int len = nums.length;
+        int[] ans = new int[len];
+        int pos = len - 1;
+        for(int i = 0, j = len -1;  i <= j; ){
+            if(nums[i] * nums[i] >= nums[j] * nums[j]){
+                ans[pos] = nums[i] * nums[i];
+                i++;
+            }else{
+                ans[pos] = nums[j] * nums[j];
+                j--;
+            }
+            pos--;
+        }
+        return ans;
+    }
+}
