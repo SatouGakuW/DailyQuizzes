d59efda739b8ce5c61a5e8ea71b8cff55f9e6343
yangshihao-arron <2867351265@qq.com>
2023-05-25 20:41:33 +0800

    add daily pratice 2023/5/25

---
+++ yshCode/src/daimasuixianglu/a5_double_pointer/d14/Q142_2.java
@@ -0,0 +1,34 @@
+package daimasuixianglu.a5_double_pointer.d14;
+
+public class Q142_2 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while(true){
+            if(fast == null || fast.next == null)
+                return null;
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow)
+                break;
+        }
+        if(fast == slow){
+            fast = head;
+            while(fast != slow){
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+        return fast;
+    }
+}
