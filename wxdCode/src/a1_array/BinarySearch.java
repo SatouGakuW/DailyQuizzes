b59b4c6cdbd4d0b0106ae2671876f2e67c5f084c
SatouGakuW <xdwwwww@qq.com>
2023-06-07 15:20:21 +0800

    Add removing elements & Modify binary search

--- wxdCode/src/a1_array/BinarySearch.java
+++ wxdCode/src/a1_array/BinarySearch.java
@@ -1,16 +1,15 @@
 package a1_array;
 
+// Leetcode Q704
 // 输出target对应的数组下标
 public class BinarySearch {
     public static void main(String[] args) {
         int[] nums = {-1, 0, 3, 5, 9, 12};
         int target = 5;
-        int ans = Solution.binarySearch(nums, target);
+        int ans = binarySearch(nums, target);
         System.out.println(ans);
     }
-}
-
-class Solution {
+    // code here
     public static int binarySearch(int[] num, int target) {
         int left = 0, right = num.length;
         while (left < right) {
---
+++ wxdCode/src/a1_array/RemovingElements.java
@@ -0,0 +1,25 @@
+package a1_array;
+
+// Leetcode Q27
+// 给定数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+public class RemovingElements {
+    public static void main(String[] args) {
+//        int[] nums = {3, 2, 2, 3};
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+//        int val = 3;
+        int val = 2;
+        int ans = rmElements(nums, val);
+        System.out.println(ans);
+    }
+    // code here
+    public static int rmElements(int[] nums, int val) {
+        int fast, slow = 0;
+        for (fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
