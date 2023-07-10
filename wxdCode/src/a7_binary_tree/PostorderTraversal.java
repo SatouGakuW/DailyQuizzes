c67fee6a814b1f51c8752238b69508d7f4c4de41
SatouGakuW <xdwwwww@qq.com>
2023-07-10 11:39:36 +0800

    Pre+Postorder Traversal

--- wxdCode/src/a7_binary_tree/PostorderTraversal.java
+++ wxdCode/src/a7_binary_tree/PostorderTraversal.java
@@ -1,16 +1,15 @@
 package a7_binary_tree;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PostorderTraversal {
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        node1.right = node2;
-        node2.left = node3;
-        System.out.println(q145s1(node1));
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(6);
+        TreeNode node1 = new TreeNode(4, node3, node4);
+        TreeNode node0 = new TreeNode(5, node1, node2);
+        System.out.println(q145s2(node0));
     }
 
     // 递归
@@ -32,5 +31,26 @@ public class PostorderTraversal {
         postorder(curr.right, vec);
         // 中
         vec.add(curr.val);
+    }
+
+    // 迭代，在前序遍历基础上修改
+    // 前序遍历的代码逻辑是 中->右->左 来模拟出 中->左->右
+    // 因此将代码逻辑变为 中->左->右 可以模拟出 中->右->左
+    // 最后将得到的数组反转就得到了 左->右->中
+    public static List<Integer> q145s2(TreeNode root) {
+        List<Integer> vec = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while (!st.isEmpty()) {
+            TreeNode curr = st.pop();
+            if (curr != null)
+                vec.add(curr.val);
+            else
+                continue;
+            st.push(curr.left);
+            st.push(curr.right);
+        }
+        Collections.reverse(vec);
+        return vec;
     }
 }
--- wxdCode/src/a7_binary_tree/PreorderTraversal.java
+++ wxdCode/src/a7_binary_tree/PreorderTraversal.java
@@ -2,17 +2,19 @@ package a7_binary_tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PreorderTraversal {
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        node1.right = node2;
-        node2.left = node3;
-        System.out.println(q144s1(node1));
+        TreeNode node4 = new TreeNode(1);
+        TreeNode node3 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(6);
+        TreeNode node1 = new TreeNode(4, node3, node4);
+        TreeNode node0 = new TreeNode(5, node1, node2);
+        System.out.println(q144s2(node0));
     }
 
+    // 递归
     public static List<Integer> q144s1(TreeNode root) {
         List<Integer> vec = new ArrayList<>();
         preorder(root, vec);
@@ -30,5 +32,25 @@ public class PreorderTraversal {
         preorder(curr.left, vec);
         // 右
         preorder(curr.right, vec);
+    }
+
+    // 迭代
+    public static List<Integer> q144s2(TreeNode root) {
+        List<Integer> vec = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        while (!st.isEmpty()) {
+            TreeNode curr = st.pop();
+            // 中
+            if (curr != null)
+                vec.add(curr.val);
+            else
+                continue;
+            // 左右节点是null的话，也会入栈，st.pop()行会将null的弹出
+            // 因此在加入vec前需要判断弹出的是否为空
+            st.push(curr.right);    // 右
+            st.push(curr.left);     // 左
+        }
+        return vec;
     }
 }
